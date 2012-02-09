a7c2fa27c6c052b8c03f50141f8c7b61068daf04
Per Andersson <dim.raven@gmail.com>
2012-02-09 18:10:18 +0100

    Added first test case to check root node attributes

---
+++ Library/Contendio.Sql.Test/SqlNode/NodeAttributeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contendio.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Contendio.Sql.Test.SqlNode
+{
+    [TestClass]
+    public class NodeAttributeTest : BaseTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            ContentInstall.Install();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            ContentInstall.Uninstall();
+        }
+
+        [TestMethod]
+        public void SqlNode_VerifyRootNodeAttribute()
+        {
+            var contentRepository = ContentRepository;
+            var rootNode = GetRootNode(contentRepository);
+            var attributes = rootNode.Attributes;
+
+            Assert.AreEqual(1, attributes.Count);
+            Assert.AreEqual("attribute:unmovable", attributes[0].Name);
+        }
+    }
+}
